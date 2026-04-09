14c8463c500032abc96a1be58115f9e76b432603
Abrar Ur Alam <safee20142@gmail.com>
2026-04-09 12:47:21 +0600

    Add line drawing feature to canvas cells

--- eas.js
+++ eas.js
@@ -1,3 +1,5 @@
+let mouseheld = false;
+
 function createCanvas(size){
     const canvasDiv = document.querySelector('#canvas');
     for(let i = 0; i < size; i++){
@@ -8,12 +10,28 @@ function createCanvas(size){
             squareDiv.style.width = `${canvasDiv.clientWidth / size}px`;
             squareDiv.style.height = `${canvasDiv.clientHeight / size}px`;
             squareDiv.style.flex = "none";
-            squareDiv.style.border = "1px solid #eee";
+            //squareDiv.style.border = "1px solid #eee";
             
+            squareDiv.addEventListener("mousedown", (e) => {
+                e.preventDefault();
+                mouseheld = true;
+                squareDiv.style.backgroundColor = "black";
+            });
+            squareDiv.addEventListener("mouseenter", () => {
+                if(mouseheld){
+                    squareDiv.style.backgroundColor = "black";
+                }
+            });
+
             canvasDiv.appendChild(squareDiv);
         }
     }
 }
 
+const html = document.querySelector('html');
+html.addEventListener("mouseup", () => {
+    mouseheld = false;
+});
 
-createCanvas(20);
+
+createCanvas(32);
